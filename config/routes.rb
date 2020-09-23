6caaf099cd59f1a9b9838e50dab316f44064b269
Kyle Geske <stungeye@gmail.com>
2020-09-23 14:43:35 -0500

    Initial Commit of Project

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # DSL - Domain Specific Language for Routing! Yay!
+
+  # GET /companies        =>   companies#index   (Load and display all companies)
+  get "/companies", to: "companies#index", as: "companies" # companies_path
+
+  # GET /companies/:id    =>   companies#show    (Load and display a single company by id)
+  get "/companies/:id", to: "companies#show", as: "company", constraints: { id: /\d+/ } # company_path
+end
